67e6988d6a07f71e06cccad854bcf38bd98660da
Gorty <gorty.44.2@gmail.com>
2018-01-20 21:36:52 +0100

    Fix issues with absence of "photos" in some albums

--- main.py
+++ main.py
@@ -11,6 +11,15 @@ def parse_args():
     parser.add_argument("--page", dest="page")
     return parser.parse_args()
 
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
 def graph_connection(token, version):
     return facebook.GraphAPI(access_token=token, version=version)
 
@@ -32,21 +41,28 @@ def page_info(graph, page, depth=0):
 
 def photos_in_album(graph, album, depth=0):
     photos_data = graph.get_object(id=album["id"], fields="photos")
-    photos_ids = [obj["id"] for obj in photos_data["photos"]["data"]]
+    pp(photos_data)
+    if "photos" in photos_data:
+        photos_ids = [obj["id"] for obj in photos_data["photos"]["data"]]
+    else:
+        return
     links = []
     for photo_id in photos_ids:
         address = graph.get_object(id=photo_id, fields="images")["images"]
         links.append(sorted(address, key=lambda x: x["height"])[-1]["source"])
     return links
 
-def mkdir_p(path):
-    try:
-        os.makedirs(path)
-    except OSError as exc:
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else:
-            raise
+def download_photos(graph, albums, page_name):
+    photos_per_album = dict()
+    for album in albums:
+        photos = photos_in_album(graph, album)
+        if photos:
+            photos_per_album[album["name"]] = photos
+    for key, value in photos_per_album.items():
+        mkdir_p("{}/{}".format(page_name, key))
+        for index, photo in enumerate(value):
+            with open("{}/{}/{}.jpg".format(page_name, key, index), "wb") as pic:
+                pic.write(requests.get(photo).content)
 
 def main():
     args = parse_args()
@@ -56,15 +72,7 @@ def main():
 
     cover, photos, albums = page_info(graph, page)
 
-    photos_per_album = dict()
-    for album in albums:
-        photos_per_album[album["name"]] = photos_in_album(graph, album)
-
-    for key, value in photos_per_album.items():
-        mkdir_p("{}/{}".format(args.page, key))
-        for index, photo in enumerate(value):
-            with open("{}/{}/{}.jpg".format(args.page, key, index), "wb") as pic:
-                pic.write(requests.get(photo).content)
+    download_photos(graph, albums, args.page)
     
 
 if __name__ == "__main__":
